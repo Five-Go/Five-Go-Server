3adbeedefa1dce76253172bb711eac1dd3c63662
KJBig <kjgi73k@gmail.com>
2023-11-22 15:38:41 +0900

    feat : Member Entity 추가
    - 로그인 관리를 위한 Member Entity 추가

--- src/main/java/com/fievgo/server/domain/Member.java
+++ src/main/java/com/fievgo/server/domain/Member.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,13 +18,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Table(name = "factor")
-public class Factor {
+@Table(name = "member")
+public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "factor_id")
+    @Column(name = "member_id")
     private Long id;
 
-    @Column(columnDefinition = "TEXT")
+    @Email
+    @Size(max = 255)
+    private String email;
+
+    @Size(max = 255)
+    private String password;
+
+    @Size(max = 255)
     private String name;
 }
