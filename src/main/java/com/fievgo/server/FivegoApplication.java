489fd18efd60c448b04b130ced5fc1f3909aa527
KJBig <kjgi73k@gmail.com>
2023-11-22 12:51:21 +0900

    refactor : Application 이름 변경

--- src/main/java/com/fievgo/server/FivegoApplication.java
+++ src/main/java/com/fievgo/server/FivegoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerApplication {
+public class FivegoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
+		SpringApplication.run(FivegoApplication.class, args);
 	}
 
 }
